23c3e65a7f51fce6d8190cc224a38f358e9ca453
keel-210 <crafters_touhou@yahoo.co.jp>
2017-06-21 01:26:20 +0900

    Ver0.02
    HighScore

---
+++ Assets/Scripts/HighScoreHolder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreHolder : MonoBehaviour
+{
+    [SerializeField]
+    Transform tra;
+    private void Update()
+    {
+        if(tra.position.y > PlayerPrefs.GetFloat("HighScore"))
+        {
+            PlayerPrefs.SetFloat("HighScore", tra.position.y);
+        }
+        Debug.Log(PlayerPrefs.GetFloat("HighScore"));
+    }
+}
---
+++ Assets/Scripts/LineAndWall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineAndWall : MonoBehaviour
+{
+    [SerializeField]
+    WallSetter ws;
+
+	void OnTriggerEnter2D(Collider2D obj)
+    {
+        ws.Set();
+	}
+}
